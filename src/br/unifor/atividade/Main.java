eaa8dc99ac033fc892e01c39ef2c0065a4a0c680
Celio Junior <celio_cjr@hotmail.com>
2022-09-26 21:05:30 -0300

    tentando ler o arquivo txt

--- src/br/unifor/atividade/Main.java
+++ src/br/unifor/atividade/Main.java
@@ -2,29 +2,37 @@ package br.unifor.atividade;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = "./entrada1.txt";
+        String filePath = "entrada1.txt";
 
-        File f1 = new File(s1);
+        File file = new File(filePath);
+
+        ArrayList<Producer> producers = new ArrayList<Producer>();
 
         try {
-            Scanner s = new Scanner(f1);
-            while(s.hasNext()) {
-                String id = s.next();
-                String name = s.next();
-                String style = s.next();
-                int teamMembers = s.nextInt();
-                int totalSongs = s.nextInt();
-                int totalAlbums = s.nextInt();
-                double payment = s.nextDouble();
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                String[] result = scanner.nextLine().split(" ");
+                if (result[7].equals("artist")) {
+                    Artist artist = new Artist(
+                            result[0],
+                            Integer.parseInt(result[3]),
+                            Integer.parseInt(result[4]),
+                            Integer.parseInt(result[5]),
+                            result[1], result[2], Double.parseDouble(result[6])
+                    );
+                    producers.add(artist);
+                }
             }
+            Artist artist = (Artist) producers.get(0);
+            System.out.println(artist.getName());
         } catch(FileNotFoundException e) {
             e.printStackTrace();
         }
-
 
     }
 }
